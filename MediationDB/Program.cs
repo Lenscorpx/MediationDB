ee52bb4ee9e84bf31c57985f8619f92af26a6286
Lens Mutombo <lensmutombo@gmail.com>
2019-10-20 18:54:50 +0200

    Debut formulaire Type conflit
    Conception de formulaire du type conflit

--- MediationDB/Program.cs
+++ MediationDB/Program.cs
@@ -17,7 +17,7 @@ namespace MediationDB
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new frm_mediateur());
+            Application.Run(new frm_type_conflit());
         }
     }
 }
