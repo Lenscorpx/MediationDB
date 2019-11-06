45a8a8126ee225f3f4608b79ae54ef38450afc70
Lens Mutombo <lensmutombo@gmail.com>
2019-11-06 17:56:23 +0200

    Conception formulaire menage
    Debut de formation de l'interface de menage

---
+++ MediationDB/ControlLibrary/uc_menage.Designer.cs
@@ -0,0 +1,45 @@
+﻿namespace MediationDB.ControlLibrary
+{
+    partial class uc_menage
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // uc_menage
+            // 
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(28)))), ((int)(((byte)(28)))), ((int)(((byte)(28)))));
+            this.Name = "uc_menage";
+            this.Size = new System.Drawing.Size(955, 680);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
