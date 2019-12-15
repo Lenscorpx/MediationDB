b774fed0947fca0db0c60aec087021d3049f0253
Lens Mutombo <lensmutombo@gmail.com>
2019-12-15 17:54:56 +0200

    Login Form Idle colors done

--- MediationDB/FormLibrary/frm_login.Designer.cs
+++ MediationDB/FormLibrary/frm_login.Designer.cs
@@ -28,11 +28,17 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frm_login));
             this.panel1 = new System.Windows.Forms.Panel();
             this.panel2 = new System.Windows.Forms.Panel();
-            this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.label1 = new System.Windows.Forms.Label();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.txt_bnf_user = new Bunifu.Framework.UI.BunifuMaterialTextbox();
+            this.txt_bnf_pwd = new Bunifu.Framework.UI.BunifuMaterialTextbox();
             this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
@@ -48,21 +54,14 @@
             // 
             // panel2
             // 
-            this.panel2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(85)))), ((int)(((byte)(142)))));
+            this.panel2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(3)))), ((int)(((byte)(43)))), ((int)(((byte)(69)))));
+            this.panel2.Controls.Add(this.txt_bnf_pwd);
+            this.panel2.Controls.Add(this.txt_bnf_user);
+            this.panel2.Controls.Add(this.pictureBox2);
             this.panel2.Location = new System.Drawing.Point(392, 22);
             this.panel2.Name = "panel2";
             this.panel2.Size = new System.Drawing.Size(450, 511);
             this.panel2.TabIndex = 2;
-            // 
-            // pictureBox1
-            // 
-            this.pictureBox1.Image = global::MediationDB.Properties.Resources.LOGO_SCC_2019_PNG;
-            this.pictureBox1.Location = new System.Drawing.Point(55, 48);
-            this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(268, 241);
-            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
-            this.pictureBox1.TabIndex = 0;
-            this.pictureBox1.TabStop = false;
             // 
             // label1
             // 
@@ -75,6 +74,64 @@
             this.label1.TabIndex = 1;
             this.label1.Text = "Mediator";
             // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = global::MediationDB.Properties.Resources.male_user_104px;
+            this.pictureBox2.Location = new System.Drawing.Point(140, 63);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(185, 153);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox2.TabIndex = 0;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::MediationDB.Properties.Resources.LOGO_SCC_2019_PNG;
+            this.pictureBox1.Location = new System.Drawing.Point(55, 48);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(268, 241);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // txt_bnf_user
+            // 
+            this.txt_bnf_user.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.txt_bnf_user.Font = new System.Drawing.Font("Century Gothic", 9.75F);
+            this.txt_bnf_user.ForeColor = System.Drawing.Color.White;
+            this.txt_bnf_user.HintForeColor = System.Drawing.Color.White;
+            this.txt_bnf_user.HintText = "Entrez votre nom d\'utilisateur";
+            this.txt_bnf_user.isPassword = false;
+            this.txt_bnf_user.LineFocusedColor = System.Drawing.Color.FromArgb(((int)(((byte)(248)))), ((int)(((byte)(72)))), ((int)(((byte)(65)))));
+            this.txt_bnf_user.LineIdleColor = System.Drawing.Color.White;
+            this.txt_bnf_user.LineMouseHoverColor = System.Drawing.Color.FromArgb(((int)(((byte)(248)))), ((int)(((byte)(72)))), ((int)(((byte)(65)))));
+            this.txt_bnf_user.LineThickness = 4;
+            this.txt_bnf_user.Location = new System.Drawing.Point(51, 236);
+            this.txt_bnf_user.Margin = new System.Windows.Forms.Padding(4);
+            this.txt_bnf_user.Name = "txt_bnf_user";
+            this.txt_bnf_user.Size = new System.Drawing.Size(370, 44);
+            this.txt_bnf_user.TabIndex = 1;
+            this.txt_bnf_user.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // txt_bnf_pwd
+            // 
+            this.txt_bnf_pwd.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.txt_bnf_pwd.Font = new System.Drawing.Font("Century Gothic", 9.75F);
+            this.txt_bnf_pwd.ForeColor = System.Drawing.Color.White;
+            this.txt_bnf_pwd.HintForeColor = System.Drawing.Color.White;
+            this.txt_bnf_pwd.HintText = "Entrez votre mot de passe";
+            this.txt_bnf_pwd.isPassword = true;
+            this.txt_bnf_pwd.LineFocusedColor = System.Drawing.Color.FromArgb(((int)(((byte)(248)))), ((int)(((byte)(72)))), ((int)(((byte)(65)))));
+            this.txt_bnf_pwd.LineIdleColor = System.Drawing.Color.White;
+            this.txt_bnf_pwd.LineMouseHoverColor = System.Drawing.Color.FromArgb(((int)(((byte)(248)))), ((int)(((byte)(72)))), ((int)(((byte)(65)))));
+            this.txt_bnf_pwd.LineThickness = 4;
+            this.txt_bnf_pwd.Location = new System.Drawing.Point(51, 324);
+            this.txt_bnf_pwd.Margin = new System.Windows.Forms.Padding(4);
+            this.txt_bnf_pwd.Name = "txt_bnf_pwd";
+            this.txt_bnf_pwd.Size = new System.Drawing.Size(370, 44);
+            this.txt_bnf_pwd.TabIndex = 2;
+            this.txt_bnf_pwd.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
             // frm_login
             // 
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
@@ -84,6 +141,7 @@
             this.Controls.Add(this.panel1);
             this.Font = new System.Drawing.Font("Century Gothic", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "frm_login";
             this.Opacity = 0.99D;
@@ -91,6 +149,8 @@
             this.TransparencyKey = System.Drawing.Color.Lime;
             this.panel1.ResumeLayout(false);
             this.panel1.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
 
@@ -102,5 +162,8 @@
         private System.Windows.Forms.Panel panel2;
         private System.Windows.Forms.PictureBox pictureBox1;
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private Bunifu.Framework.UI.BunifuMaterialTextbox txt_bnf_pwd;
+        private Bunifu.Framework.UI.BunifuMaterialTextbox txt_bnf_user;
     }
 }
--- MediationDB/Properties/Resources.Designer.cs
+++ MediationDB/Properties/Resources.Designer.cs
@@ -233,6 +233,16 @@ namespace MediationDB.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
+        internal static System.Drawing.Bitmap male_user_104px {
+            get {
+                object obj = ResourceManager.GetObject("male_user_104px", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
         internal static System.Drawing.Bitmap mediation {
             get {
                 object obj = ResourceManager.GetObject("mediation", resourceCulture);
