9bb349610432a685e0106232949d05e086aa3baa
Lens Mutombo <lensmutombo@gmail.com>
2019-12-15 17:39:30 +0200

    Ajout du logo SCC dans les ressources
    Application aux icones du projet

--- MediationDB/FormLibrary/frm_login.Designer.cs
+++ MediationDB/FormLibrary/frm_login.Designer.cs
@@ -31,6 +31,7 @@
             this.panel1 = new System.Windows.Forms.Panel();
             this.panel2 = new System.Windows.Forms.Panel();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
             this.panel1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
@@ -38,36 +39,47 @@
             // panel1
             // 
             this.panel1.BackColor = System.Drawing.Color.White;
+            this.panel1.Controls.Add(this.label1);
             this.panel1.Controls.Add(this.pictureBox1);
-            this.panel1.Location = new System.Drawing.Point(86, 69);
+            this.panel1.Location = new System.Drawing.Point(12, 155);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(1159, 432);
+            this.panel1.Size = new System.Drawing.Size(830, 349);
             this.panel1.TabIndex = 0;
             // 
             // panel2
             // 
             this.panel2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(85)))), ((int)(((byte)(142)))));
-            this.panel2.Location = new System.Drawing.Point(756, 12);
+            this.panel2.Location = new System.Drawing.Point(392, 22);
             this.panel2.Name = "panel2";
             this.panel2.Size = new System.Drawing.Size(450, 511);
             this.panel2.TabIndex = 2;
             // 
             // pictureBox1
             // 
-            this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.pictureBox1.Image = global::MediationDB.Properties.Resources.Feronia_portada;
-            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Image = global::MediationDB.Properties.Resources.LOGO_SCC_2019_PNG;
+            this.pictureBox1.Location = new System.Drawing.Point(55, 48);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(1159, 432);
+            this.pictureBox1.Size = new System.Drawing.Size(268, 241);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
-            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabIndex = 0;
             this.pictureBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Century Gothic", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(85)))), ((int)(((byte)(142)))));
+            this.label1.Location = new System.Drawing.Point(127, 292);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(121, 30);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Mediator";
             // 
             // frm_login
             // 
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
             this.BackColor = System.Drawing.Color.Lime;
-            this.ClientSize = new System.Drawing.Size(1249, 535);
+            this.ClientSize = new System.Drawing.Size(899, 552);
             this.Controls.Add(this.panel2);
             this.Controls.Add(this.panel1);
             this.Font = new System.Drawing.Font("Century Gothic", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
@@ -78,6 +90,7 @@
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.TransparencyKey = System.Drawing.Color.Lime;
             this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
 
@@ -88,5 +101,6 @@
         private System.Windows.Forms.Panel panel1;
         private System.Windows.Forms.Panel panel2;
         private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
     }
 }
--- MediationDB/Properties/Resources.Designer.cs
+++ MediationDB/Properties/Resources.Designer.cs
@@ -223,6 +223,16 @@ namespace MediationDB.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
+        internal static System.Drawing.Bitmap LOGO_SCC_2019_PNG {
+            get {
+                object obj = ResourceManager.GetObject("LOGO-SCC-2019-PNG", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
         internal static System.Drawing.Bitmap mediation {
             get {
                 object obj = ResourceManager.GetObject("mediation", resourceCulture);
