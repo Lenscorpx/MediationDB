d64e5bdff9188d5ed32570ecb88285896b15a1b5
Lens Mutombo <lensmutombo@gmail.com>
2019-12-22 20:34:25 +0200

    Update Data_Repository.cs
    Suppression inutiles

--- MediationDB/DataLibrary/Data_Repository.cs
+++ MediationDB/DataLibrary/Data_Repository.cs
@@ -2013,36 +2013,7 @@ namespace MediationDB.DataLibrary
             }
         }
         
-        public void charts_nombre_filles(Label nombre_filles)
-        {
-            cnx = new SqlConnection(prms.ToString());
-            try
-            {
-                if (cnx.State == ConnectionState.Closed)
-                    cnx.Open();
-                var cmd = new SqlCommand("charts_nombre_filles", cnx)
-                {
-                    CommandType = CommandType.StoredProcedure
-                };
-                cmd.ExecuteNonQuery();
-                var da = new SqlDataAdapter(cmd);
-                var dt = new DataTable();
-                da.Fill(dt);
-                foreach (DataRow dr in dt.Rows)
-                {
-                    nombre_filles.Text = Convert.ToString(dr[0]);
-                    cnx.Close(); cnx.Dispose();
-                }
-            }
-            catch (Exception tdf)
-            {
-                MessageBox.Show("Connection failed!\n" + tdf);
-            }
-            finally
-            {
-                cnx.Close(); cnx.Dispose();
-            }
-        }
+        
         public void charts_nombre_garcons(Label nombre_garcons)
         {
             cnx = new SqlConnection(prms.ToString());
