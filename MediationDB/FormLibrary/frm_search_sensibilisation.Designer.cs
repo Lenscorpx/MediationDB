cfebef5586b57edc93c007f6aaee422a1bd4c503
Lens Mutombo <lensmutombo@gmail.com>
2020-01-05 18:24:44 +0200

    Update frm_search_sensibilisation.Designer.cs
    Test

--- MediationDB/FormLibrary/frm_search_sensibilisation.Designer.cs
+++ MediationDB/FormLibrary/frm_search_sensibilisation.Designer.cs
@@ -34,7 +34,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1083, 510);
+            this.ClientSize = new System.Drawing.Size(1227, 510);
             this.Name = "frm_search_sensibilisation";
             this.Text = "frm_search_sensibilisation";
             this.ResumeLayout(false);
