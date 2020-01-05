0bc88feea418888934e8c8cddc3e0f46eba5423d
Lens Mutombo <lensmutombo@gmail.com>
2020-01-05 23:14:51 +0200

    Suppression inutile

--- MediationDB/DataLibrary/Data_Repository.cs
+++ MediationDB/DataLibrary/Data_Repository.cs
@@ -8,7 +8,6 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-//using DevExpress.XtraPrinting.Preview;
 using MediationDB.FormLibrary;
 
 namespace MediationDB.DataLibrary
