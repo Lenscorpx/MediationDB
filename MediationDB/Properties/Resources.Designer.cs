a7a33d7c9a7b38249b7c1feeda06d4aa03bdbfaa
Lens Mutombo <lensmutombo@gmail.com>
2020-01-13 01:14:30 +0200

    Type atelier icon added

--- MediationDB/Properties/Resources.Designer.cs
+++ MediationDB/Properties/Resources.Designer.cs
@@ -563,6 +563,16 @@ namespace MediationDB.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
+        internal static System.Drawing.Bitmap schedule_104px {
+            get {
+                object obj = ResourceManager.GetObject("schedule_104px", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
         internal static System.Drawing.Bitmap sensibilisation {
             get {
                 object obj = ResourceManager.GetObject("sensibilisation", resourceCulture);
