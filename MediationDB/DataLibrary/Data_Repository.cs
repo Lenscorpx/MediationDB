4c9a6a7267112f544f7958c980406e5ef0fedf67
Lens Mutombo <lensmutombo@gmail.com>
2020-01-12 21:57:03 +0200

    Update Data_Repository.cs
    Ajout des afficher_ateliers_sensibilises

--- MediationDB/DataLibrary/Data_Repository.cs
+++ MediationDB/DataLibrary/Data_Repository.cs
@@ -31,37 +31,7 @@ namespace MediationDB.DataLibrary
             //prms.Mot_de_passe = "Server@2019.com?";
             
         }
-        public void afficher_mediateur(DataGridView dtg)
-        {
-            cnx = new SqlConnection(prms.ToString());
-            try
-            {
-                if (cnx.State == ConnectionState.Closed)
-                    cnx.Open();
-                var cmd = new SqlCommand("afficher_mediateur", cnx)
-                {
-                    CommandType = CommandType.StoredProcedure
-                };
-                cmd.ExecuteNonQuery();
-                var da = new SqlDataAdapter(cmd);
-                var dt = new DataTable();
-                da.Fill(dt);
-                dtg.DataSource = dt;
-            }
-            catch (Exception exct)
-            {
-                var rs = new DialogResult();
-                rs = MessageBox.Show("Want to see error code?", "Errors ", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
-                if (rs == DialogResult.Yes)
-                {
-                    MessageBox.Show(exct.ToString());
-                }
-            }
-            finally
-            {
-                cnx.Close(); cnx.Dispose();
-            }
-        }
+        c
         public void enregistrer_mediateur(string id_mediateur, string descr_mediateur)
         {
             cnx = new SqlConnection(prms.ToString());
@@ -1982,6 +1952,38 @@ namespace MediationDB.DataLibrary
                     CommandType = CommandType.StoredProcedure
                 };
                 cmd.Parameters.Add(new SqlParameter("id_localite", SqlDbType.NVarChar)).Value = id_localite;
+                cmd.ExecuteNonQuery();
+                var da = new SqlDataAdapter(cmd);
+                var dt = new DataTable();
+                da.Fill(dt);
+                dtg.DataSource = dt;
+            }
+            catch (Exception exct)
+            {
+                var rs = new DialogResult();
+                rs = MessageBox.Show("Want to see error code?", "Errors ", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+                if (rs == DialogResult.Yes)
+                {
+                    MessageBox.Show(exct.ToString());
+                }
+            }
+            finally
+            {
+                cnx.Close(); cnx.Dispose();
+            }
+        }
+
+        public void afficher_ateliers_sensibilises(DataGridView dtg)
+        {
+            cnx = new SqlConnection(prms.ToString());
+            try
+            {
+                if (cnx.State == ConnectionState.Closed)
+                    cnx.Open();
+                var cmd = new SqlCommand("afficher_ateliers_sensibilises", cnx)
+                {
+                    CommandType = CommandType.StoredProcedure
+                };
                 cmd.ExecuteNonQuery();
                 var da = new SqlDataAdapter(cmd);
                 var dt = new DataTable();
