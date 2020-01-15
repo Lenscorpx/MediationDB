0b7a0d9edd9704fbcac8d575e1c1bcd4101bef3b
Lens Mutombo <lensmutombo@gmail.com>
2020-01-15 23:40:08 +0200

    ajustement

--- MediationDB/FormLibrary/frm_atelier.Designer.cs
+++ MediationDB/FormLibrary/frm_atelier.Designer.cs
@@ -64,7 +64,7 @@
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Century Gothic", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.ForeColor = System.Drawing.Color.White;
-            this.label1.Location = new System.Drawing.Point(342, 9);
+            this.label1.Location = new System.Drawing.Point(343, 9);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(390, 36);
             this.label1.TabIndex = 2;
