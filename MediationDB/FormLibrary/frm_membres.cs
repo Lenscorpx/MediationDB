9b89f58f535f0bf63c73b94ffd181ac5bd6ce10e
Lens Mutombo <lensmutombo@gmail.com>
2020-01-21 01:33:52 +0200

    dynamisation des crud sur le formulaire
    dynamisation des cruds sur le formulaire

--- MediationDB/FormLibrary/frm_membres.cs
+++ MediationDB/FormLibrary/frm_membres.cs
@@ -7,14 +7,29 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using MediationDB.DataLibrary;
 
 namespace MediationDB.FormLibrary
 {
     public partial class frm_membres : Form
     {
+        Data_Repository rps = new Data_Repository();
         public frm_membres()
         {
             InitializeComponent();
+            refresh();
+        }
+        public void refresh()
+        {
+            rps.afficher_membre(bunifuCustomDataGrid2);
+            rps.recuperer_vulnerabilite(cbx_vulnerabilite);
+            txt_adresse.ResetText();
+            txt_code_membre.ResetText();
+            txt_id_menage.ResetText();
+            txt_noms_membre.ResetText();
+            txt_profession.ResetText();
+            txt_provenance.ResetText();
+            txt_telephone.ResetText();            
         }
 
         private void bunifuImageButton1_Click(object sender, EventArgs e)
@@ -29,7 +44,52 @@ namespace MediationDB.FormLibrary
 
         private void btn_enregistrer_Click(object sender, EventArgs e)
         {
+            if (txt_code_membre.Text == "")
+            {
+                if (txt_id_menage.Text ==""||txt_noms_membre.Text==""||cbx_sexe.Text==""||cbx_etat_civil.Text==""||cbx_vulnerabilite.Text=="")
+                {
+                    MessageBox.Show("Veuillez completer les champs importants!");
+                }
+                else
+                {
+                    if(rd_btn_yes.Checked==true)
+                    {
+                        rps.inserer_membre(txt_code_membre.Text, txt_noms_membre.Text, Convert.ToDateTime(dt_date_naissance.Text),
+                                       cbx_sexe.Text, cbx_etat_civil.Text, cbx_vulnerabilite.Text, txt_provenance.Text, 
+                                       txt_adresse.Text, txt_telephone.Text, "Yes", txt_profession.Text, txt_id_menage.Text, DateTime.Now);
+                    }
+                    else
+                    {
+                        rps.inserer_membre(txt_code_membre.Text, txt_noms_membre.Text, Convert.ToDateTime(dt_date_naissance.Text),
+                                       cbx_sexe.Text, cbx_etat_civil.Text, cbx_vulnerabilite.Text, txt_provenance.Text,
+                                       txt_adresse.Text, txt_telephone.Text, "No", txt_profession.Text, txt_id_menage.Text, DateTime.Now);
+                    }
+                    
+                }
+            }
+            else
+            {
+                if (txt_id_menage.Text == "" || txt_noms_membre.Text == "" || cbx_sexe.Text == "" || cbx_etat_civil.Text == "" || cbx_vulnerabilite.Text == "")
+                {
+                    MessageBox.Show("Veuillez completer les champs importants!");
+                }
+                else
+                {
+                    if (rd_btn_yes.Checked == true)
+                    {
+                        rps.modifier_membre(txt_code_membre.Text, txt_noms_membre.Text, Convert.ToDateTime(dt_date_naissance.Text),
+                                       cbx_sexe.Text, cbx_etat_civil.Text, cbx_vulnerabilite.Text, txt_provenance.Text,
+                                       txt_adresse.Text, txt_telephone.Text, "Yes", txt_profession.Text, txt_id_menage.Text, DateTime.Now);
+                    }
+                    else
+                    {
+                        rps.modifier_membre(txt_code_membre.Text, txt_noms_membre.Text, Convert.ToDateTime(dt_date_naissance.Text),
+                                       cbx_sexe.Text, cbx_etat_civil.Text, cbx_vulnerabilite.Text, txt_provenance.Text,
+                                       txt_adresse.Text, txt_telephone.Text, "No", txt_profession.Text, txt_id_menage.Text, DateTime.Now);
+                    }
 
+                }
+            }
         }
 
         private void frm_membres_Load(object sender, EventArgs e)
