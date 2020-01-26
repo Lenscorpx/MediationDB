037504f66b46c2933fd58e5731feb767702d5168
Lens Mutombo <lensmutombo@gmail.com>
2020-01-26 23:30:22 +0200

    Update Program.cs

--- MediationDB/Program.cs
+++ MediationDB/Program.cs
@@ -17,7 +17,7 @@ namespace MediationDB
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new frm_evolution());
+            Application.Run(new frm_menu());
         }
     }
 }
