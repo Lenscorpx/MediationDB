9e3f39e5230f0efe58a52fdaf2ad40b4012aa94f
Lens Mutombo <lensmutombo@gmail.com>
2020-02-01 23:16:15 +0200

    Changement size Splash

--- MediationDB/FormLibrary/frm_splash.Designer.cs
+++ MediationDB/FormLibrary/frm_splash.Designer.cs
@@ -35,10 +35,10 @@
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 17F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(3)))), ((int)(((byte)(43)))), ((int)(((byte)(69)))));
-            this.ClientSize = new System.Drawing.Size(982, 563);
+            this.ClientSize = new System.Drawing.Size(1250, 563);
             this.Font = new System.Drawing.Font("Century Gothic", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
-            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "frm_splash";
             this.ResumeLayout(false);
 
