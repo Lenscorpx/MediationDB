0b2e8a0e3b6102854d2707a1931f5665456b4ce3
Lens Mutombo <lensmutombo@gmail.com>
2020-02-02 23:39:12 +0200

    frm_distribution added
    Changement de font pour frfm_distribution

--- MediationDB/FormLibrary/frm_distribution.Designer.cs
+++ MediationDB/FormLibrary/frm_distribution.Designer.cs
@@ -28,10 +28,19 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // frm_distribution
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 17F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1067, 588);
+            this.Font = new System.Drawing.Font("Century Gothic", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "frm_distribution";
             this.Text = "frm_distribution";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
