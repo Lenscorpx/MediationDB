02d0c11cb7c2e36823f9cee8b4c86d58248c4859
Lens Mutombo <lensmutombo@gmail.com>
2020-02-05 12:25:03 +0200

    Debut creation formulaire Assignation bailleurs

---
+++ MediationDB/FormLibrary/frm_assignation_bailleurs.Designer.cs
@@ -0,0 +1,46 @@
+﻿namespace MediationDB.FormLibrary
+{
+    partial class frm_assignation_bailleurs
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // frm_assignation_bailleurs
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1260, 450);
+            this.Name = "frm_assignation_bailleurs";
+            this.Text = "frm_assignation_bailleurs";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
