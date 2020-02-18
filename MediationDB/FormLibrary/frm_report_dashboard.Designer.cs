63371193eec8d4de26e66930a6b1615a1378b182
Lens Mutombo <lensmutombo@gmail.com>
2020-02-18 16:13:48 +0200

    panel container added on form

--- MediationDB/FormLibrary/frm_report_dashboard.Designer.cs
+++ MediationDB/FormLibrary/frm_report_dashboard.Designer.cs
@@ -28,14 +28,43 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.pnl_menu = new System.Windows.Forms.Panel();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.pnl_container = new System.Windows.Forms.Panel();
             this.SuspendLayout();
+            // 
+            // pnl_menu
+            // 
+            this.pnl_menu.Dock = System.Windows.Forms.DockStyle.Left;
+            this.pnl_menu.Location = new System.Drawing.Point(0, 0);
+            this.pnl_menu.Name = "pnl_menu";
+            this.pnl_menu.Size = new System.Drawing.Size(230, 714);
+            this.pnl_menu.TabIndex = 0;
+            // 
+            // panel1
+            // 
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(230, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1070, 43);
+            this.panel1.TabIndex = 1;
+            // 
+            // pnl_container
+            // 
+            this.pnl_container.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pnl_container.Location = new System.Drawing.Point(230, 43);
+            this.pnl_container.Name = "pnl_container";
+            this.pnl_container.Size = new System.Drawing.Size(1070, 671);
+            this.pnl_container.TabIndex = 2;
             // 
             // frm_report_dashboard
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 17F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(3)))), ((int)(((byte)(43)))), ((int)(((byte)(69)))));
             this.ClientSize = new System.Drawing.Size(1300, 714);
+            this.Controls.Add(this.pnl_container);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.pnl_menu);
             this.Font = new System.Drawing.Font("Century Gothic", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
@@ -49,5 +78,9 @@
         }
 
         #endregion
+
+        private System.Windows.Forms.Panel pnl_menu;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel pnl_container;
     }
 }
