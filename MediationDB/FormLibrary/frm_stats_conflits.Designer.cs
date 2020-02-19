f7af12d45bd510030ad62820b2786153bb1a4972
Lens Mutombo <lensmutombo@gmail.com>
2020-02-19 10:35:33 +0200

    Ajout des titres sur stats_conflits

--- MediationDB/FormLibrary/frm_stats_conflits.Designer.cs
+++ MediationDB/FormLibrary/frm_stats_conflits.Designer.cs
@@ -29,87 +29,45 @@
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frm_stats_conflits));
-            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
-            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
-            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
-            this.pnl_menu = new System.Windows.Forms.Panel();
             this.pnl_header = new System.Windows.Forms.Panel();
-            this.panel1 = new System.Windows.Forms.Panel();
-            this.label3 = new System.Windows.Forms.Label();
-            this.label4 = new System.Windows.Forms.Label();
-            this.lbl_nbre_conflits = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
-            this.bunifuProgressBar1 = new Bunifu.Framework.UI.BunifuProgressBar();
             this.bunifuGauge1 = new Bunifu.Framework.UI.BunifuGauge();
-            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lbl_nbre_conflits = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.bunifuImageButton1 = new Bunifu.Framework.UI.BunifuImageButton();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pnl_header.SuspendLayout();
             this.panel1.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bunifuImageButton1)).BeginInit();
             this.SuspendLayout();
-            // 
-            // pnl_menu
-            // 
-            this.pnl_menu.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(248)))), ((int)(((byte)(72)))), ((int)(((byte)(65)))));
-            this.pnl_menu.Dock = System.Windows.Forms.DockStyle.Left;
-            this.pnl_menu.Location = new System.Drawing.Point(0, 0);
-            this.pnl_menu.Name = "pnl_menu";
-            this.pnl_menu.Size = new System.Drawing.Size(200, 714);
-            this.pnl_menu.TabIndex = 36;
             // 
             // pnl_header
             // 
+            this.pnl_header.Controls.Add(this.bunifuImageButton1);
+            this.pnl_header.Controls.Add(this.label1);
             this.pnl_header.Dock = System.Windows.Forms.DockStyle.Top;
-            this.pnl_header.Location = new System.Drawing.Point(200, 0);
+            this.pnl_header.Location = new System.Drawing.Point(0, 0);
             this.pnl_header.Name = "pnl_header";
-            this.pnl_header.Size = new System.Drawing.Size(1100, 53);
+            this.pnl_header.Size = new System.Drawing.Size(1300, 53);
             this.pnl_header.TabIndex = 37;
             // 
-            // panel1
+            // bunifuGauge1
             // 
-            this.panel1.Controls.Add(this.chart1);
-            this.panel1.Controls.Add(this.label3);
-            this.panel1.Controls.Add(this.label4);
-            this.panel1.Controls.Add(this.lbl_nbre_conflits);
-            this.panel1.Controls.Add(this.label2);
-            this.panel1.Controls.Add(this.bunifuProgressBar1);
-            this.panel1.Controls.Add(this.bunifuGauge1);
-            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.panel1.Location = new System.Drawing.Point(200, 53);
-            this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(1100, 661);
-            this.panel1.TabIndex = 38;
-            // 
-            // label3
-            // 
-            this.label3.AutoSize = true;
-            this.label3.Font = new System.Drawing.Font("Century Gothic", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.ForeColor = System.Drawing.Color.White;
-            this.label3.Location = new System.Drawing.Point(298, 109);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(26, 30);
-            this.label3.TabIndex = 44;
-            this.label3.Text = "0";
-            // 
-            // label4
-            // 
-            this.label4.AutoSize = true;
-            this.label4.Font = new System.Drawing.Font("Century Gothic", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label4.ForeColor = System.Drawing.Color.White;
-            this.label4.Location = new System.Drawing.Point(94, 108);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(162, 30);
-            this.label4.TabIndex = 43;
-            this.label4.Text = "Total conflits";
-            // 
-            // lbl_nbre_conflits
-            // 
-            this.lbl_nbre_conflits.AutoSize = true;
-            this.lbl_nbre_conflits.Font = new System.Drawing.Font("Century Gothic", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lbl_nbre_conflits.ForeColor = System.Drawing.Color.White;
-            this.lbl_nbre_conflits.Location = new System.Drawing.Point(230, 19);
-            this.lbl_nbre_conflits.Name = "lbl_nbre_conflits";
-            this.lbl_nbre_conflits.Size = new System.Drawing.Size(26, 30);
-            this.lbl_nbre_conflits.TabIndex = 42;
-            this.lbl_nbre_conflits.Text = "0";
+            this.bunifuGauge1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("bunifuGauge1.BackgroundImage")));
+            this.bunifuGauge1.Font = new System.Drawing.Font("Century Gothic", 10F);
+            this.bunifuGauge1.ForeColor = System.Drawing.Color.White;
+            this.bunifuGauge1.Location = new System.Drawing.Point(99, 153);
+            this.bunifuGauge1.Margin = new System.Windows.Forms.Padding(5);
+            this.bunifuGauge1.Name = "bunifuGauge1";
+            this.bunifuGauge1.ProgressBgColor = System.Drawing.Color.White;
+            this.bunifuGauge1.ProgressColor1 = System.Drawing.Color.FromArgb(((int)(((byte)(248)))), ((int)(((byte)(72)))), ((int)(((byte)(65)))));
+            this.bunifuGauge1.ProgressColor2 = System.Drawing.Color.FromArgb(((int)(((byte)(248)))), ((int)(((byte)(72)))), ((int)(((byte)(65)))));
+            this.bunifuGauge1.Size = new System.Drawing.Size(165, 112);
+            this.bunifuGauge1.TabIndex = 39;
+            this.bunifuGauge1.Thickness = 30;
+            this.bunifuGauge1.Value = 78;
             // 
             // label2
             // 
@@ -122,52 +80,75 @@
             this.label2.TabIndex = 41;
             this.label2.Text = "Total conflits";
             // 
-            // bunifuProgressBar1
+            // lbl_nbre_conflits
             // 
-            this.bunifuProgressBar1.BackColor = System.Drawing.Color.Silver;
-            this.bunifuProgressBar1.BorderRadius = 5;
-            this.bunifuProgressBar1.Location = new System.Drawing.Point(771, 406);
-            this.bunifuProgressBar1.Margin = new System.Windows.Forms.Padding(5, 5, 5, 5);
-            this.bunifuProgressBar1.MaximumValue = 100;
-            this.bunifuProgressBar1.Name = "bunifuProgressBar1";
-            this.bunifuProgressBar1.ProgressColor = System.Drawing.Color.Teal;
-            this.bunifuProgressBar1.Size = new System.Drawing.Size(577, 13);
-            this.bunifuProgressBar1.TabIndex = 40;
-            this.bunifuProgressBar1.Value = 0;
+            this.lbl_nbre_conflits.AutoSize = true;
+            this.lbl_nbre_conflits.Font = new System.Drawing.Font("Century Gothic", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_nbre_conflits.ForeColor = System.Drawing.Color.White;
+            this.lbl_nbre_conflits.Location = new System.Drawing.Point(230, 19);
+            this.lbl_nbre_conflits.Name = "lbl_nbre_conflits";
+            this.lbl_nbre_conflits.Size = new System.Drawing.Size(26, 30);
+            this.lbl_nbre_conflits.TabIndex = 42;
+            this.lbl_nbre_conflits.Text = "0";
             // 
-            // bunifuGauge1
+            // label4
             // 
-            this.bunifuGauge1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("bunifuGauge1.BackgroundImage")));
-            this.bunifuGauge1.Font = new System.Drawing.Font("Century Gothic", 10F);
-            this.bunifuGauge1.ForeColor = System.Drawing.Color.White;
-            this.bunifuGauge1.Location = new System.Drawing.Point(99, 153);
-            this.bunifuGauge1.Margin = new System.Windows.Forms.Padding(5, 5, 5, 5);
-            this.bunifuGauge1.Name = "bunifuGauge1";
-            this.bunifuGauge1.ProgressBgColor = System.Drawing.Color.White;
-            this.bunifuGauge1.ProgressColor1 = System.Drawing.Color.FromArgb(((int)(((byte)(248)))), ((int)(((byte)(72)))), ((int)(((byte)(65)))));
-            this.bunifuGauge1.ProgressColor2 = System.Drawing.Color.FromArgb(((int)(((byte)(248)))), ((int)(((byte)(72)))), ((int)(((byte)(65)))));
-            this.bunifuGauge1.Size = new System.Drawing.Size(165, 112);
-            this.bunifuGauge1.TabIndex = 39;
-            this.bunifuGauge1.Thickness = 30;
-            this.bunifuGauge1.Value = 78;
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Century Gothic", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.Color.White;
+            this.label4.Location = new System.Drawing.Point(94, 108);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(162, 30);
+            this.label4.TabIndex = 43;
+            this.label4.Text = "Total conflits";
             // 
-            // chart1
+            // label3
             // 
-            this.chart1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(3)))), ((int)(((byte)(43)))), ((int)(((byte)(69)))));
-            chartArea1.Name = "ChartArea1";
-            this.chart1.ChartAreas.Add(chartArea1);
-            legend1.Name = "Legend1";
-            this.chart1.Legends.Add(legend1);
-            this.chart1.Location = new System.Drawing.Point(372, 35);
-            this.chart1.Name = "chart1";
-            series1.ChartArea = "ChartArea1";
-            series1.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Pie;
-            series1.Legend = "Legend1";
-            series1.Name = "Series1";
-            this.chart1.Series.Add(series1);
-            this.chart1.Size = new System.Drawing.Size(393, 316);
-            this.chart1.TabIndex = 45;
-            this.chart1.Text = "chart1";
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Century Gothic", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.White;
+            this.label3.Location = new System.Drawing.Point(298, 109);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(26, 30);
+            this.label3.TabIndex = 44;
+            this.label3.Text = "0";
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.label4);
+            this.panel1.Controls.Add(this.lbl_nbre_conflits);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.bunifuGauge1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.Location = new System.Drawing.Point(0, 53);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1300, 661);
+            this.panel1.TabIndex = 38;
+            // 
+            // bunifuImageButton1
+            // 
+            this.bunifuImageButton1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(3)))), ((int)(((byte)(43)))), ((int)(((byte)(69)))));
+            this.bunifuImageButton1.Image = global::MediationDB.Properties.Resources.Fermer_window;
+            this.bunifuImageButton1.ImageActive = null;
+            this.bunifuImageButton1.Location = new System.Drawing.Point(1246, 7);
+            this.bunifuImageButton1.Name = "bunifuImageButton1";
+            this.bunifuImageButton1.Size = new System.Drawing.Size(46, 40);
+            this.bunifuImageButton1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.bunifuImageButton1.TabIndex = 8;
+            this.bunifuImageButton1.TabStop = false;
+            this.bunifuImageButton1.Zoom = 10;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Century Gothic", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(407, 7);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(491, 36);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Informations sur les sensibilisations";
             // 
             // frm_stats_conflits
             // 
@@ -176,30 +157,29 @@
             this.ClientSize = new System.Drawing.Size(1300, 714);
             this.Controls.Add(this.panel1);
             this.Controls.Add(this.pnl_header);
-            this.Controls.Add(this.pnl_menu);
             this.Font = new System.Drawing.Font("Century Gothic", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "frm_stats_conflits";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.pnl_header.ResumeLayout(false);
+            this.pnl_header.PerformLayout();
             this.panel1.ResumeLayout(false);
             this.panel1.PerformLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bunifuImageButton1)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
-
-        private System.Windows.Forms.Panel pnl_menu;
         private System.Windows.Forms.Panel pnl_header;
-        private System.Windows.Forms.Panel panel1;
-        private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.Label label4;
-        private System.Windows.Forms.Label lbl_nbre_conflits;
-        private System.Windows.Forms.Label label2;
-        private Bunifu.Framework.UI.BunifuProgressBar bunifuProgressBar1;
         private Bunifu.Framework.UI.BunifuGauge bunifuGauge1;
-        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label lbl_nbre_conflits;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Panel panel1;
+        private Bunifu.Framework.UI.BunifuImageButton bunifuImageButton1;
+        private System.Windows.Forms.Label label1;
     }
 }
