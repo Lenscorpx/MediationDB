0037d89ad27d6eb46e7273c295393e5ad398881a
Lens Mutombo <lensmutombo@gmail.com>
2020-02-19 13:23:10 +0200

    added procedures for filles

--- Database Codes/db_mediator.sql
+++ Database Codes/db_mediator.sql
@@ -2645,9 +2645,10 @@ create procedure stats_femmes_conflit_resolus_parperiode
 		where
 			id_resolution like @id_resolution and t_mediation.date_debut_mediation between @date_un and @date_deux
 go
-create procedure stats_total_filles_conflit_parperiode
+create procedure stats_filles_conflit_resolus_parperiode
 @date_un date,
-@date_deux date
+@date_deux date,
+@id_resolution nvarchar(50)
 	as
 		select  
 			--count(distinct t_conflit.num_conflit)      
@@ -2672,7 +2673,7 @@ create procedure stats_total_filles_conflit_parperiode
 				inner join
             		t_menages on t_parties.id_menage = t_menages.id_menage
 		where
-			t_mediation.date_debut_mediation between @date_un and @date_deux
+			id_resolution like @id_resolution and t_mediation.date_debut_mediation between @date_un and @date_deux
 go
 create procedure stats_total_garcons_conflit_parperiode
 @date_un date,
