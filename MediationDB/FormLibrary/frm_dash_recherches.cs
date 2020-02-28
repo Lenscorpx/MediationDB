d5415ec4f3921208061d4f4571c2fb63ca471171
Lens Mutombo <lensmutombo@gmail.com>
2020-02-28 09:53:43 +0200

    Added frm_dash_recherche
    Added search for dashboard

---
+++ MediationDB/FormLibrary/frm_dash_recherches.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MediationDB.FormLibrary
+{
+    public partial class frm_dash_recherches : Form
+    {
+        public frm_dash_recherches()
+        {
+            InitializeComponent();
+        }
+    }
+}
