79dee7719b58d86dab51ea20798fbda1956151a6
Lens Mutombo <lensmutombo@gmail.com>
2020-02-28 09:54:12 +0200

    Dash resized

--- MediationDB/FormLibrary/frm_dash_recherches.Designer.cs
+++ MediationDB/FormLibrary/frm_dash_recherches.Designer.cs
@@ -28,10 +28,17 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // frm_dash_recherches
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(933, 450);
+            this.Name = "frm_dash_recherches";
             this.Text = "frm_dash_recherches";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
