4b32d98b88a2e06f547b62e12146befb001660b5
Lens Mutombo <lensmutombo@gmail.com>
2020-02-28 11:04:56 +0200

    Update frm_beneficiaires.Designer.cs
    Added components

--- MediationDB/FormLibrary/frm_beneficiaires.Designer.cs
+++ MediationDB/FormLibrary/frm_beneficiaires.Designer.cs
@@ -31,6 +31,16 @@
             this.panel1 = new System.Windows.Forms.Panel();
             this.label1 = new System.Windows.Forms.Label();
             this.bunifuImageButton1 = new Bunifu.Framework.UI.BunifuImageButton();
+            this.txt_id_beneficiaire = new Bunifu.Framework.UI.BunifuMaterialTextbox();
+            this.txt_noms = new Bunifu.Framework.UI.BunifuMaterialTextbox();
+            this.btn_supprimer = new Bunifu.Framework.UI.BunifuFlatButton();
+            this.btn_enregistrer = new Bunifu.Framework.UI.BunifuFlatButton();
+            this.dt_date_distribution = new MetroFramework.Controls.MetroDateTime();
+            this.cbx_sexe = new MetroFramework.Controls.MetroComboBox();
+            this.cbx_vulnerabilite = new MetroFramework.Controls.MetroComboBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txt_adresse = new Bunifu.Framework.UI.BunifuMaterialTextbox();
+            this.txt_telephone = new Bunifu.Framework.UI.BunifuMaterialTextbox();
             this.panel1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.bunifuImageButton1)).BeginInit();
             this.SuspendLayout();
@@ -73,12 +83,238 @@
             this.bunifuImageButton1.Zoom = 10;
             this.bunifuImageButton1.Click += new System.EventHandler(this.bunifuImageButton1_Click);
             // 
+            // txt_id_beneficiaire
+            // 
+            this.txt_id_beneficiaire.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.txt_id_beneficiaire.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.txt_id_beneficiaire.Font = new System.Drawing.Font("Century Gothic", 9.75F);
+            this.txt_id_beneficiaire.ForeColor = System.Drawing.Color.White;
+            this.txt_id_beneficiaire.HintForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.txt_id_beneficiaire.HintText = "Code du beneficiaire";
+            this.txt_id_beneficiaire.isPassword = false;
+            this.txt_id_beneficiaire.LineFocusedColor = System.Drawing.Color.FromArgb(((int)(((byte)(248)))), ((int)(((byte)(72)))), ((int)(((byte)(65)))));
+            this.txt_id_beneficiaire.LineIdleColor = System.Drawing.Color.White;
+            this.txt_id_beneficiaire.LineMouseHoverColor = System.Drawing.Color.FromArgb(((int)(((byte)(248)))), ((int)(((byte)(72)))), ((int)(((byte)(65)))));
+            this.txt_id_beneficiaire.LineThickness = 4;
+            this.txt_id_beneficiaire.Location = new System.Drawing.Point(33, 59);
+            this.txt_id_beneficiaire.Margin = new System.Windows.Forms.Padding(4);
+            this.txt_id_beneficiaire.Name = "txt_id_beneficiaire";
+            this.txt_id_beneficiaire.Size = new System.Drawing.Size(359, 44);
+            this.txt_id_beneficiaire.TabIndex = 78;
+            this.txt_id_beneficiaire.TextAlign = System.Windows.Forms.HorizontalAlignment.Left;
+            // 
+            // txt_noms
+            // 
+            this.txt_noms.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.txt_noms.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.txt_noms.Font = new System.Drawing.Font("Century Gothic", 9.75F);
+            this.txt_noms.ForeColor = System.Drawing.Color.White;
+            this.txt_noms.HintForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.txt_noms.HintText = "Noms du beneficiaire";
+            this.txt_noms.isPassword = false;
+            this.txt_noms.LineFocusedColor = System.Drawing.Color.FromArgb(((int)(((byte)(248)))), ((int)(((byte)(72)))), ((int)(((byte)(65)))));
+            this.txt_noms.LineIdleColor = System.Drawing.Color.White;
+            this.txt_noms.LineMouseHoverColor = System.Drawing.Color.FromArgb(((int)(((byte)(248)))), ((int)(((byte)(72)))), ((int)(((byte)(65)))));
+            this.txt_noms.LineThickness = 4;
+            this.txt_noms.Location = new System.Drawing.Point(33, 111);
+            this.txt_noms.Margin = new System.Windows.Forms.Padding(4);
+            this.txt_noms.Name = "txt_noms";
+            this.txt_noms.Size = new System.Drawing.Size(359, 44);
+            this.txt_noms.TabIndex = 79;
+            this.txt_noms.TextAlign = System.Windows.Forms.HorizontalAlignment.Left;
+            // 
+            // btn_supprimer
+            // 
+            this.btn_supprimer.Activecolor = System.Drawing.Color.FromArgb(((int)(((byte)(248)))), ((int)(((byte)(72)))), ((int)(((byte)(65)))));
+            this.btn_supprimer.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.btn_supprimer.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(248)))), ((int)(((byte)(72)))), ((int)(((byte)(65)))));
+            this.btn_supprimer.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btn_supprimer.BorderRadius = 0;
+            this.btn_supprimer.ButtonText = "Supprimer";
+            this.btn_supprimer.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btn_supprimer.DisabledColor = System.Drawing.Color.Gray;
+            this.btn_supprimer.Font = new System.Drawing.Font("Century Gothic", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_supprimer.ForeColor = System.Drawing.Color.White;
+            this.btn_supprimer.Iconcolor = System.Drawing.Color.Transparent;
+            this.btn_supprimer.Iconimage = global::MediationDB.Properties.Resources.add_trash_96px;
+            this.btn_supprimer.Iconimage_right = null;
+            this.btn_supprimer.Iconimage_right_Selected = null;
+            this.btn_supprimer.Iconimage_Selected = null;
+            this.btn_supprimer.IconMarginLeft = 0;
+            this.btn_supprimer.IconMarginRight = 0;
+            this.btn_supprimer.IconRightVisible = true;
+            this.btn_supprimer.IconRightZoom = 0D;
+            this.btn_supprimer.IconVisible = true;
+            this.btn_supprimer.IconZoom = 50D;
+            this.btn_supprimer.IsTab = true;
+            this.btn_supprimer.Location = new System.Drawing.Point(1098, 177);
+            this.btn_supprimer.Margin = new System.Windows.Forms.Padding(5, 6, 5, 6);
+            this.btn_supprimer.Name = "btn_supprimer";
+            this.btn_supprimer.Normalcolor = System.Drawing.Color.FromArgb(((int)(((byte)(248)))), ((int)(((byte)(72)))), ((int)(((byte)(65)))));
+            this.btn_supprimer.OnHovercolor = System.Drawing.Color.FromArgb(((int)(((byte)(3)))), ((int)(((byte)(43)))), ((int)(((byte)(69)))));
+            this.btn_supprimer.OnHoverTextColor = System.Drawing.Color.White;
+            this.btn_supprimer.selected = false;
+            this.btn_supprimer.Size = new System.Drawing.Size(164, 44);
+            this.btn_supprimer.TabIndex = 86;
+            this.btn_supprimer.Text = "Supprimer";
+            this.btn_supprimer.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.btn_supprimer.Textcolor = System.Drawing.Color.White;
+            this.btn_supprimer.TextFont = new System.Drawing.Font("Century Gothic", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            // 
+            // btn_enregistrer
+            // 
+            this.btn_enregistrer.Activecolor = System.Drawing.Color.FromArgb(((int)(((byte)(248)))), ((int)(((byte)(72)))), ((int)(((byte)(65)))));
+            this.btn_enregistrer.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.btn_enregistrer.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(248)))), ((int)(((byte)(72)))), ((int)(((byte)(65)))));
+            this.btn_enregistrer.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btn_enregistrer.BorderRadius = 0;
+            this.btn_enregistrer.ButtonText = "Enregistrer";
+            this.btn_enregistrer.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btn_enregistrer.DisabledColor = System.Drawing.Color.Gray;
+            this.btn_enregistrer.Font = new System.Drawing.Font("Century Gothic", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_enregistrer.ForeColor = System.Drawing.Color.White;
+            this.btn_enregistrer.Iconcolor = System.Drawing.Color.Transparent;
+            this.btn_enregistrer.Iconimage = global::MediationDB.Properties.Resources.add_96px;
+            this.btn_enregistrer.Iconimage_right = null;
+            this.btn_enregistrer.Iconimage_right_Selected = null;
+            this.btn_enregistrer.Iconimage_Selected = null;
+            this.btn_enregistrer.IconMarginLeft = 0;
+            this.btn_enregistrer.IconMarginRight = 0;
+            this.btn_enregistrer.IconRightVisible = true;
+            this.btn_enregistrer.IconRightZoom = 0D;
+            this.btn_enregistrer.IconVisible = true;
+            this.btn_enregistrer.IconZoom = 50D;
+            this.btn_enregistrer.IsTab = true;
+            this.btn_enregistrer.Location = new System.Drawing.Point(924, 177);
+            this.btn_enregistrer.Margin = new System.Windows.Forms.Padding(5, 6, 5, 6);
+            this.btn_enregistrer.Name = "btn_enregistrer";
+            this.btn_enregistrer.Normalcolor = System.Drawing.Color.FromArgb(((int)(((byte)(248)))), ((int)(((byte)(72)))), ((int)(((byte)(65)))));
+            this.btn_enregistrer.OnHovercolor = System.Drawing.Color.FromArgb(((int)(((byte)(3)))), ((int)(((byte)(43)))), ((int)(((byte)(69)))));
+            this.btn_enregistrer.OnHoverTextColor = System.Drawing.Color.White;
+            this.btn_enregistrer.selected = false;
+            this.btn_enregistrer.Size = new System.Drawing.Size(164, 44);
+            this.btn_enregistrer.TabIndex = 85;
+            this.btn_enregistrer.Text = "Enregistrer";
+            this.btn_enregistrer.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.btn_enregistrer.Textcolor = System.Drawing.Color.White;
+            this.btn_enregistrer.TextFont = new System.Drawing.Font("Century Gothic", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            // 
+            // dt_date_distribution
+            // 
+            this.dt_date_distribution.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.dt_date_distribution.Location = new System.Drawing.Point(1030, 69);
+            this.dt_date_distribution.MinimumSize = new System.Drawing.Size(0, 29);
+            this.dt_date_distribution.Name = "dt_date_distribution";
+            this.dt_date_distribution.Size = new System.Drawing.Size(232, 29);
+            this.dt_date_distribution.Style = MetroFramework.MetroColorStyle.Blue;
+            this.dt_date_distribution.TabIndex = 84;
+            this.dt_date_distribution.Theme = MetroFramework.MetroThemeStyle.Dark;
+            // 
+            // cbx_sexe
+            // 
+            this.cbx_sexe.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.cbx_sexe.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(28)))), ((int)(((byte)(28)))), ((int)(((byte)(28)))));
+            this.cbx_sexe.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.cbx_sexe.ForeColor = System.Drawing.Color.White;
+            this.cbx_sexe.FormattingEnabled = true;
+            this.cbx_sexe.ItemHeight = 23;
+            this.cbx_sexe.Items.AddRange(new object[] {
+            "Feminin",
+            "Masculin"});
+            this.cbx_sexe.Location = new System.Drawing.Point(412, 74);
+            this.cbx_sexe.Name = "cbx_sexe";
+            this.cbx_sexe.PromptText = "Choisissez le genre ici";
+            this.cbx_sexe.Size = new System.Drawing.Size(198, 29);
+            this.cbx_sexe.Style = MetroFramework.MetroColorStyle.Blue;
+            this.cbx_sexe.TabIndex = 83;
+            this.cbx_sexe.Theme = MetroFramework.MetroThemeStyle.Dark;
+            this.cbx_sexe.UseSelectable = true;
+            // 
+            // cbx_vulnerabilite
+            // 
+            this.cbx_vulnerabilite.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.cbx_vulnerabilite.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(28)))), ((int)(((byte)(28)))), ((int)(((byte)(28)))));
+            this.cbx_vulnerabilite.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.cbx_vulnerabilite.ForeColor = System.Drawing.Color.White;
+            this.cbx_vulnerabilite.FormattingEnabled = true;
+            this.cbx_vulnerabilite.ItemHeight = 23;
+            this.cbx_vulnerabilite.Location = new System.Drawing.Point(616, 74);
+            this.cbx_vulnerabilite.Name = "cbx_vulnerabilite";
+            this.cbx_vulnerabilite.PromptText = "Vulnerabilite";
+            this.cbx_vulnerabilite.Size = new System.Drawing.Size(254, 29);
+            this.cbx_vulnerabilite.Style = MetroFramework.MetroColorStyle.Blue;
+            this.cbx_vulnerabilite.TabIndex = 87;
+            this.cbx_vulnerabilite.Theme = MetroFramework.MetroThemeStyle.Dark;
+            this.cbx_vulnerabilite.UseSelectable = true;
+            // 
+            // label3
+            // 
+            this.label3.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Century Gothic", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.White;
+            this.label3.Location = new System.Drawing.Point(885, 77);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(139, 19);
+            this.label3.TabIndex = 88;
+            this.label3.Text = "Date de naissance";
+            // 
+            // txt_adresse
+            // 
+            this.txt_adresse.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.txt_adresse.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.txt_adresse.Font = new System.Drawing.Font("Century Gothic", 9.75F);
+            this.txt_adresse.ForeColor = System.Drawing.Color.White;
+            this.txt_adresse.HintForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.txt_adresse.HintText = "Completez l\'adresse ici!";
+            this.txt_adresse.isPassword = false;
+            this.txt_adresse.LineFocusedColor = System.Drawing.Color.FromArgb(((int)(((byte)(248)))), ((int)(((byte)(72)))), ((int)(((byte)(65)))));
+            this.txt_adresse.LineIdleColor = System.Drawing.Color.White;
+            this.txt_adresse.LineMouseHoverColor = System.Drawing.Color.FromArgb(((int)(((byte)(248)))), ((int)(((byte)(72)))), ((int)(((byte)(65)))));
+            this.txt_adresse.LineThickness = 4;
+            this.txt_adresse.Location = new System.Drawing.Point(412, 111);
+            this.txt_adresse.Margin = new System.Windows.Forms.Padding(4);
+            this.txt_adresse.Name = "txt_adresse";
+            this.txt_adresse.Size = new System.Drawing.Size(458, 44);
+            this.txt_adresse.TabIndex = 89;
+            this.txt_adresse.TextAlign = System.Windows.Forms.HorizontalAlignment.Left;
+            // 
+            // txt_telephone
+            // 
+            this.txt_telephone.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.txt_telephone.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.txt_telephone.Font = new System.Drawing.Font("Century Gothic", 9.75F);
+            this.txt_telephone.ForeColor = System.Drawing.Color.White;
+            this.txt_telephone.HintForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.txt_telephone.HintText = "Numero de telephone";
+            this.txt_telephone.isPassword = false;
+            this.txt_telephone.LineFocusedColor = System.Drawing.Color.FromArgb(((int)(((byte)(248)))), ((int)(((byte)(72)))), ((int)(((byte)(65)))));
+            this.txt_telephone.LineIdleColor = System.Drawing.Color.White;
+            this.txt_telephone.LineMouseHoverColor = System.Drawing.Color.FromArgb(((int)(((byte)(248)))), ((int)(((byte)(72)))), ((int)(((byte)(65)))));
+            this.txt_telephone.LineThickness = 4;
+            this.txt_telephone.Location = new System.Drawing.Point(889, 111);
+            this.txt_telephone.Margin = new System.Windows.Forms.Padding(4);
+            this.txt_telephone.Name = "txt_telephone";
+            this.txt_telephone.Size = new System.Drawing.Size(373, 44);
+            this.txt_telephone.TabIndex = 90;
+            this.txt_telephone.TextAlign = System.Windows.Forms.HorizontalAlignment.Left;
+            // 
             // frm_beneficiaires
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 17F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(3)))), ((int)(((byte)(43)))), ((int)(((byte)(69)))));
             this.ClientSize = new System.Drawing.Size(1300, 714);
+            this.Controls.Add(this.txt_telephone);
+            this.Controls.Add(this.txt_adresse);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.cbx_vulnerabilite);
+            this.Controls.Add(this.btn_supprimer);
+            this.Controls.Add(this.btn_enregistrer);
+            this.Controls.Add(this.dt_date_distribution);
+            this.Controls.Add(this.cbx_sexe);
+            this.Controls.Add(this.txt_noms);
+            this.Controls.Add(this.txt_id_beneficiaire);
             this.Controls.Add(this.panel1);
             this.Font = new System.Drawing.Font("Century Gothic", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
@@ -89,6 +325,7 @@
             this.panel1.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)(this.bunifuImageButton1)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -97,5 +334,15 @@
         private System.Windows.Forms.Panel panel1;
         private System.Windows.Forms.Label label1;
         private Bunifu.Framework.UI.BunifuImageButton bunifuImageButton1;
+        public Bunifu.Framework.UI.BunifuMaterialTextbox txt_id_beneficiaire;
+        public Bunifu.Framework.UI.BunifuMaterialTextbox txt_noms;
+        private Bunifu.Framework.UI.BunifuFlatButton btn_supprimer;
+        private Bunifu.Framework.UI.BunifuFlatButton btn_enregistrer;
+        private MetroFramework.Controls.MetroDateTime dt_date_distribution;
+        private MetroFramework.Controls.MetroComboBox cbx_sexe;
+        private MetroFramework.Controls.MetroComboBox cbx_vulnerabilite;
+        private System.Windows.Forms.Label label3;
+        public Bunifu.Framework.UI.BunifuMaterialTextbox txt_adresse;
+        public Bunifu.Framework.UI.BunifuMaterialTextbox txt_telephone;
     }
 }
