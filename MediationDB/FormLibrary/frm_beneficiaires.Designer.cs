5f32a105fbe410757491688ec93d0135bfca62b8
Lens Mutombo <lensmutombo@gmail.com>
2020-02-28 10:10:12 +0200

    Added frm_beneficiaires
    Beneficiaires ajoutes

---
+++ MediationDB/FormLibrary/frm_beneficiaires.Designer.cs
@@ -0,0 +1,46 @@
+﻿namespace MediationDB.FormLibrary
+{
+    partial class frm_beneficiaires
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // frm_beneficiaires
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(954, 450);
+            this.Name = "frm_beneficiaires";
+            this.Text = "frm_beneficiaires";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
