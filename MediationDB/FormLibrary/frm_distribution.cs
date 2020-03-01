d8e3dcc4b253bd1a3517c74d9e5c367342f44f71
Lens Mutombo <lensmutombo@gmail.com>
2020-03-01 23:45:17 +0200

    Update frm_distribution.cs
    Ajout de l'event sur agr

--- MediationDB/FormLibrary/frm_distribution.cs
+++ MediationDB/FormLibrary/frm_distribution.cs
@@ -24,6 +24,7 @@ namespace MediationDB.FormLibrary
             rps.recuperer_projet(cbx_projet);
             rps.recuperer_executants(cbx_executant);
             rps.recuperer_localite(listBox1);
+            rps.recuperer_agr(cbx_agr);
             rps.afficher_distribution(bunifuCustomDataGrid2);
         }
 
