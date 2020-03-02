cd13d91a72407f142ab0fc43d70191c922d897c8
Lens Mutombo <lensmutombo@gmail.com>
2020-03-02 12:52:42 +0200

    Update db_mediator.sql
    Ajout de recherche par date de distribution

--- Database Codes/db_mediator.sql
+++ Database Codes/db_mediator.sql
@@ -2086,6 +2086,24 @@ as
 		id_projet like '%'+@id_projet+'%'
 	order by code_distribution desc
 go
+create procedure rechercher_distrtibution_pardate
+@date_distribution nvarchar(200)
+as
+	select top 50
+		code_distribution as 'Code',
+		date_distribution as 'Date',
+		id_localite as 'Lieu',
+		id_projet as 'Projet',
+		id_agr as 'AGR',
+		qte as 'Qte',
+		valeur as 'Valeur',
+		id_executant as 'Executant',
+		observation as 'Observation'
+	from t_distribution
+	where
+		date_distribution like '%'+@date_distribution+'%'
+	order by code_distribution desc
+go
 --------------------------------------Fin codes distribution---------------------------------------------
 create table t_assignation_beneficiaires
 (
