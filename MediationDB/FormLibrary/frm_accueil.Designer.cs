6ecffb5a04a09c096eef7e65ebe9ab1c1c9c900a
Lens Mutombo <lensmutombo@gmail.com>
2020-03-03 14:45:48 +0200

    Update frm_accueil.Designer.cs
    Changed title to land data gateway

--- MediationDB/FormLibrary/frm_accueil.Designer.cs
+++ MediationDB/FormLibrary/frm_accueil.Designer.cs
@@ -71,11 +71,11 @@
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Century Gothic", 30F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.ForeColor = System.Drawing.Color.White;
-            this.label1.Location = new System.Drawing.Point(305, 9);
+            this.label1.Location = new System.Drawing.Point(307, 9);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(718, 49);
+            this.label1.Size = new System.Drawing.Size(686, 49);
             this.label1.TabIndex = 8;
-            this.label1.Text = "UN Habitat - Land Datas Dashboard";
+            this.label1.Text = "UN Habitat - Land Datas Gateway";
             // 
             // groupBox1
             // 
