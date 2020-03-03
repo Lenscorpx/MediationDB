f886d4a003be089163c84874fca2fd44872b7098
Lens Mutombo <lensmutombo@gmail.com>
2020-03-03 14:32:49 +0200

    Changed title to UN

--- MediationDB/FormLibrary/frm_accueil.Designer.cs
+++ MediationDB/FormLibrary/frm_accueil.Designer.cs
@@ -73,9 +73,9 @@
             this.label1.ForeColor = System.Drawing.Color.White;
             this.label1.Location = new System.Drawing.Point(305, 9);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(753, 49);
+            this.label1.Size = new System.Drawing.Size(718, 49);
             this.label1.TabIndex = 8;
-            this.label1.Text = "ONU Habitat - Land Datas Dashboard";
+            this.label1.Text = "UN Habitat - Land Datas Dashboard";
             // 
             // groupBox1
             // 
