dfc73c02106cde52809dae1d72c5cda12752d4fe
Lens Mutombo <lensmutombo@gmail.com>
2020-03-04 23:38:46 +0200

    Update frm_beneficiaires.cs
    Ajout de l'evenenement appelant le membre afficher_vulnerabilite dans le combobox

--- MediationDB/FormLibrary/frm_beneficiaires.cs
+++ MediationDB/FormLibrary/frm_beneficiaires.cs
@@ -7,16 +7,22 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using MediationDB.DataLibrary;
 
 namespace MediationDB.FormLibrary
 {
     public partial class frm_beneficiaires : Form
     {
+        Data_Repository rps = new Data_Repository();
         public frm_beneficiaires()
         {
             InitializeComponent();
         }
-
+        private void refresh()
+        {
+            rps.afficher_beneficiaires(bunifuCustomDataGrid2);
+            rps.recuperer_vulnerabilite(cbx_vulnerabilite);
+        }
         private void bunifuImageButton1_Click(object sender, EventArgs e)
         {
             this.Close();
