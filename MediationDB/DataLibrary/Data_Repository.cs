82bf2c72d33ce76c00980cd04f176973453de03e
Lens Mutombo <lensmutombo@gmail.com>
2020-03-05 15:20:55 +0200

    Update Data_Repository.cs

--- MediationDB/DataLibrary/Data_Repository.cs
+++ MediationDB/DataLibrary/Data_Repository.cs
@@ -37,6 +37,10 @@ namespace MediationDB.DataLibrary
             //prms.Base_de_donnees = "db_foncier";
             //prms.Nom_user = "sa";
             //prms.Mot_de_passe = "UNH@PKA2020";
+            //prms.Serveur = "DESKTOP-NFR9KTI";
+            //prms.Base_de_donnees = "db_foncier";
+            //prms.Nom_user = "sa";
+            //prms.Mot_de_passe = "123456789";
 
         }
         public void afficher_mediateur(DataGridView dtg)
