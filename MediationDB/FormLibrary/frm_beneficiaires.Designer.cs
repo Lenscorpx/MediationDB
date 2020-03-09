57bfe9118c85650a56212b49178f94b59e921e33
Lens Mutombo <lensmutombo@gmail.com>
2020-03-09 16:45:38 +0200

    Update frm_beneficiaires.Designer.cs

--- MediationDB/FormLibrary/frm_beneficiaires.Designer.cs
+++ MediationDB/FormLibrary/frm_beneficiaires.Designer.cs
@@ -35,11 +35,8 @@
             System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle5 = new System.Windows.Forms.DataGridViewCellStyle();
             this.panel1 = new System.Windows.Forms.Panel();
             this.label1 = new System.Windows.Forms.Label();
-            this.bunifuImageButton1 = new Bunifu.Framework.UI.BunifuImageButton();
             this.txt_id_beneficiaire = new Bunifu.Framework.UI.BunifuMaterialTextbox();
             this.txt_noms = new Bunifu.Framework.UI.BunifuMaterialTextbox();
-            this.btn_supprimer = new Bunifu.Framework.UI.BunifuFlatButton();
-            this.btn_enregistrer = new Bunifu.Framework.UI.BunifuFlatButton();
             this.dt_date_distribution = new MetroFramework.Controls.MetroDateTime();
             this.cbx_sexe = new MetroFramework.Controls.MetroComboBox();
             this.cbx_vulnerabilite = new MetroFramework.Controls.MetroComboBox();
@@ -47,14 +44,20 @@
             this.txt_adresse = new Bunifu.Framework.UI.BunifuMaterialTextbox();
             this.txt_telephone = new Bunifu.Framework.UI.BunifuMaterialTextbox();
             this.bunifuCustomDataGrid2 = new Bunifu.Framework.UI.BunifuCustomDataGrid();
+            this.btn_supprimer = new Bunifu.Framework.UI.BunifuFlatButton();
+            this.btn_enregistrer = new Bunifu.Framework.UI.BunifuFlatButton();
+            this.bunifuImageButton1 = new Bunifu.Framework.UI.BunifuImageButton();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.panel1.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.bunifuImageButton1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.bunifuCustomDataGrid2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bunifuImageButton1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // panel1
             // 
             this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(3)))), ((int)(((byte)(43)))), ((int)(((byte)(69)))));
+            this.panel1.Controls.Add(this.pictureBox1);
             this.panel1.Controls.Add(this.label1);
             this.panel1.Controls.Add(this.bunifuImageButton1);
             this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
@@ -74,21 +77,6 @@
             this.label1.Size = new System.Drawing.Size(480, 30);
             this.label1.TabIndex = 8;
             this.label1.Text = "Informations sur les bénéficiaires d\'AGR";
-            // 
-            // bunifuImageButton1
-            // 
-            this.bunifuImageButton1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
-            this.bunifuImageButton1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(3)))), ((int)(((byte)(43)))), ((int)(((byte)(69)))));
-            this.bunifuImageButton1.Image = global::MediationDB.Properties.Resources.multiply_96px;
-            this.bunifuImageButton1.ImageActive = null;
-            this.bunifuImageButton1.Location = new System.Drawing.Point(1250, 7);
-            this.bunifuImageButton1.Name = "bunifuImageButton1";
-            this.bunifuImageButton1.Size = new System.Drawing.Size(42, 34);
-            this.bunifuImageButton1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
-            this.bunifuImageButton1.TabIndex = 7;
-            this.bunifuImageButton1.TabStop = false;
-            this.bunifuImageButton1.Zoom = 10;
-            this.bunifuImageButton1.Click += new System.EventHandler(this.bunifuImageButton1_Click);
             // 
             // txt_id_beneficiaire
             // 
@@ -129,82 +117,6 @@
             this.txt_noms.Size = new System.Drawing.Size(359, 44);
             this.txt_noms.TabIndex = 79;
             this.txt_noms.TextAlign = System.Windows.Forms.HorizontalAlignment.Left;
-            // 
-            // btn_supprimer
-            // 
-            this.btn_supprimer.Activecolor = System.Drawing.Color.FromArgb(((int)(((byte)(248)))), ((int)(((byte)(72)))), ((int)(((byte)(65)))));
-            this.btn_supprimer.Anchor = System.Windows.Forms.AnchorStyles.None;
-            this.btn_supprimer.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(248)))), ((int)(((byte)(72)))), ((int)(((byte)(65)))));
-            this.btn_supprimer.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.btn_supprimer.BorderRadius = 0;
-            this.btn_supprimer.ButtonText = "Supprimer";
-            this.btn_supprimer.Cursor = System.Windows.Forms.Cursors.Hand;
-            this.btn_supprimer.DisabledColor = System.Drawing.Color.Gray;
-            this.btn_supprimer.Font = new System.Drawing.Font("Century Gothic", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btn_supprimer.ForeColor = System.Drawing.Color.White;
-            this.btn_supprimer.Iconcolor = System.Drawing.Color.Transparent;
-            this.btn_supprimer.Iconimage = global::MediationDB.Properties.Resources.add_trash_96px;
-            this.btn_supprimer.Iconimage_right = null;
-            this.btn_supprimer.Iconimage_right_Selected = null;
-            this.btn_supprimer.Iconimage_Selected = null;
-            this.btn_supprimer.IconMarginLeft = 0;
-            this.btn_supprimer.IconMarginRight = 0;
-            this.btn_supprimer.IconRightVisible = true;
-            this.btn_supprimer.IconRightZoom = 0D;
-            this.btn_supprimer.IconVisible = true;
-            this.btn_supprimer.IconZoom = 50D;
-            this.btn_supprimer.IsTab = true;
-            this.btn_supprimer.Location = new System.Drawing.Point(1098, 165);
-            this.btn_supprimer.Margin = new System.Windows.Forms.Padding(5, 6, 5, 6);
-            this.btn_supprimer.Name = "btn_supprimer";
-            this.btn_supprimer.Normalcolor = System.Drawing.Color.FromArgb(((int)(((byte)(248)))), ((int)(((byte)(72)))), ((int)(((byte)(65)))));
-            this.btn_supprimer.OnHovercolor = System.Drawing.Color.FromArgb(((int)(((byte)(3)))), ((int)(((byte)(43)))), ((int)(((byte)(69)))));
-            this.btn_supprimer.OnHoverTextColor = System.Drawing.Color.White;
-            this.btn_supprimer.selected = false;
-            this.btn_supprimer.Size = new System.Drawing.Size(164, 44);
-            this.btn_supprimer.TabIndex = 86;
-            this.btn_supprimer.Text = "Supprimer";
-            this.btn_supprimer.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
-            this.btn_supprimer.Textcolor = System.Drawing.Color.White;
-            this.btn_supprimer.TextFont = new System.Drawing.Font("Century Gothic", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            // 
-            // btn_enregistrer
-            // 
-            this.btn_enregistrer.Activecolor = System.Drawing.Color.FromArgb(((int)(((byte)(248)))), ((int)(((byte)(72)))), ((int)(((byte)(65)))));
-            this.btn_enregistrer.Anchor = System.Windows.Forms.AnchorStyles.None;
-            this.btn_enregistrer.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(248)))), ((int)(((byte)(72)))), ((int)(((byte)(65)))));
-            this.btn_enregistrer.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.btn_enregistrer.BorderRadius = 0;
-            this.btn_enregistrer.ButtonText = "Enregistrer";
-            this.btn_enregistrer.Cursor = System.Windows.Forms.Cursors.Hand;
-            this.btn_enregistrer.DisabledColor = System.Drawing.Color.Gray;
-            this.btn_enregistrer.Font = new System.Drawing.Font("Century Gothic", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btn_enregistrer.ForeColor = System.Drawing.Color.White;
-            this.btn_enregistrer.Iconcolor = System.Drawing.Color.Transparent;
-            this.btn_enregistrer.Iconimage = global::MediationDB.Properties.Resources.add_96px;
-            this.btn_enregistrer.Iconimage_right = null;
-            this.btn_enregistrer.Iconimage_right_Selected = null;
-            this.btn_enregistrer.Iconimage_Selected = null;
-            this.btn_enregistrer.IconMarginLeft = 0;
-            this.btn_enregistrer.IconMarginRight = 0;
-            this.btn_enregistrer.IconRightVisible = true;
-            this.btn_enregistrer.IconRightZoom = 0D;
-            this.btn_enregistrer.IconVisible = true;
-            this.btn_enregistrer.IconZoom = 50D;
-            this.btn_enregistrer.IsTab = true;
-            this.btn_enregistrer.Location = new System.Drawing.Point(924, 165);
-            this.btn_enregistrer.Margin = new System.Windows.Forms.Padding(5, 6, 5, 6);
-            this.btn_enregistrer.Name = "btn_enregistrer";
-            this.btn_enregistrer.Normalcolor = System.Drawing.Color.FromArgb(((int)(((byte)(248)))), ((int)(((byte)(72)))), ((int)(((byte)(65)))));
-            this.btn_enregistrer.OnHovercolor = System.Drawing.Color.FromArgb(((int)(((byte)(3)))), ((int)(((byte)(43)))), ((int)(((byte)(69)))));
-            this.btn_enregistrer.OnHoverTextColor = System.Drawing.Color.White;
-            this.btn_enregistrer.selected = false;
-            this.btn_enregistrer.Size = new System.Drawing.Size(164, 44);
-            this.btn_enregistrer.TabIndex = 85;
-            this.btn_enregistrer.Text = "Enregistrer";
-            this.btn_enregistrer.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
-            this.btn_enregistrer.Textcolor = System.Drawing.Color.White;
-            this.btn_enregistrer.TextFont = new System.Drawing.Font("Century Gothic", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             // 
             // dt_date_distribution
             // 
@@ -366,6 +278,107 @@
             this.bunifuCustomDataGrid2.Size = new System.Drawing.Size(1300, 496);
             this.bunifuCustomDataGrid2.TabIndex = 91;
             // 
+            // btn_supprimer
+            // 
+            this.btn_supprimer.Activecolor = System.Drawing.Color.FromArgb(((int)(((byte)(248)))), ((int)(((byte)(72)))), ((int)(((byte)(65)))));
+            this.btn_supprimer.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.btn_supprimer.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(248)))), ((int)(((byte)(72)))), ((int)(((byte)(65)))));
+            this.btn_supprimer.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btn_supprimer.BorderRadius = 0;
+            this.btn_supprimer.ButtonText = "Supprimer";
+            this.btn_supprimer.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btn_supprimer.DisabledColor = System.Drawing.Color.Gray;
+            this.btn_supprimer.Font = new System.Drawing.Font("Century Gothic", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_supprimer.ForeColor = System.Drawing.Color.White;
+            this.btn_supprimer.Iconcolor = System.Drawing.Color.Transparent;
+            this.btn_supprimer.Iconimage = global::MediationDB.Properties.Resources.add_trash_96px;
+            this.btn_supprimer.Iconimage_right = null;
+            this.btn_supprimer.Iconimage_right_Selected = null;
+            this.btn_supprimer.Iconimage_Selected = null;
+            this.btn_supprimer.IconMarginLeft = 0;
+            this.btn_supprimer.IconMarginRight = 0;
+            this.btn_supprimer.IconRightVisible = true;
+            this.btn_supprimer.IconRightZoom = 0D;
+            this.btn_supprimer.IconVisible = true;
+            this.btn_supprimer.IconZoom = 50D;
+            this.btn_supprimer.IsTab = true;
+            this.btn_supprimer.Location = new System.Drawing.Point(1098, 165);
+            this.btn_supprimer.Margin = new System.Windows.Forms.Padding(5, 6, 5, 6);
+            this.btn_supprimer.Name = "btn_supprimer";
+            this.btn_supprimer.Normalcolor = System.Drawing.Color.FromArgb(((int)(((byte)(248)))), ((int)(((byte)(72)))), ((int)(((byte)(65)))));
+            this.btn_supprimer.OnHovercolor = System.Drawing.Color.FromArgb(((int)(((byte)(3)))), ((int)(((byte)(43)))), ((int)(((byte)(69)))));
+            this.btn_supprimer.OnHoverTextColor = System.Drawing.Color.White;
+            this.btn_supprimer.selected = false;
+            this.btn_supprimer.Size = new System.Drawing.Size(164, 44);
+            this.btn_supprimer.TabIndex = 86;
+            this.btn_supprimer.Text = "Supprimer";
+            this.btn_supprimer.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.btn_supprimer.Textcolor = System.Drawing.Color.White;
+            this.btn_supprimer.TextFont = new System.Drawing.Font("Century Gothic", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            // 
+            // btn_enregistrer
+            // 
+            this.btn_enregistrer.Activecolor = System.Drawing.Color.FromArgb(((int)(((byte)(248)))), ((int)(((byte)(72)))), ((int)(((byte)(65)))));
+            this.btn_enregistrer.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.btn_enregistrer.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(248)))), ((int)(((byte)(72)))), ((int)(((byte)(65)))));
+            this.btn_enregistrer.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btn_enregistrer.BorderRadius = 0;
+            this.btn_enregistrer.ButtonText = "Enregistrer";
+            this.btn_enregistrer.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btn_enregistrer.DisabledColor = System.Drawing.Color.Gray;
+            this.btn_enregistrer.Font = new System.Drawing.Font("Century Gothic", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_enregistrer.ForeColor = System.Drawing.Color.White;
+            this.btn_enregistrer.Iconcolor = System.Drawing.Color.Transparent;
+            this.btn_enregistrer.Iconimage = global::MediationDB.Properties.Resources.add_96px;
+            this.btn_enregistrer.Iconimage_right = null;
+            this.btn_enregistrer.Iconimage_right_Selected = null;
+            this.btn_enregistrer.Iconimage_Selected = null;
+            this.btn_enregistrer.IconMarginLeft = 0;
+            this.btn_enregistrer.IconMarginRight = 0;
+            this.btn_enregistrer.IconRightVisible = true;
+            this.btn_enregistrer.IconRightZoom = 0D;
+            this.btn_enregistrer.IconVisible = true;
+            this.btn_enregistrer.IconZoom = 50D;
+            this.btn_enregistrer.IsTab = true;
+            this.btn_enregistrer.Location = new System.Drawing.Point(924, 165);
+            this.btn_enregistrer.Margin = new System.Windows.Forms.Padding(5, 6, 5, 6);
+            this.btn_enregistrer.Name = "btn_enregistrer";
+            this.btn_enregistrer.Normalcolor = System.Drawing.Color.FromArgb(((int)(((byte)(248)))), ((int)(((byte)(72)))), ((int)(((byte)(65)))));
+            this.btn_enregistrer.OnHovercolor = System.Drawing.Color.FromArgb(((int)(((byte)(3)))), ((int)(((byte)(43)))), ((int)(((byte)(69)))));
+            this.btn_enregistrer.OnHoverTextColor = System.Drawing.Color.White;
+            this.btn_enregistrer.selected = false;
+            this.btn_enregistrer.Size = new System.Drawing.Size(164, 44);
+            this.btn_enregistrer.TabIndex = 85;
+            this.btn_enregistrer.Text = "Enregistrer";
+            this.btn_enregistrer.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.btn_enregistrer.Textcolor = System.Drawing.Color.White;
+            this.btn_enregistrer.TextFont = new System.Drawing.Font("Century Gothic", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            // 
+            // bunifuImageButton1
+            // 
+            this.bunifuImageButton1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.bunifuImageButton1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(3)))), ((int)(((byte)(43)))), ((int)(((byte)(69)))));
+            this.bunifuImageButton1.Image = global::MediationDB.Properties.Resources.multiply_96px;
+            this.bunifuImageButton1.ImageActive = null;
+            this.bunifuImageButton1.Location = new System.Drawing.Point(1250, 7);
+            this.bunifuImageButton1.Name = "bunifuImageButton1";
+            this.bunifuImageButton1.Size = new System.Drawing.Size(42, 34);
+            this.bunifuImageButton1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.bunifuImageButton1.TabIndex = 7;
+            this.bunifuImageButton1.TabStop = false;
+            this.bunifuImageButton1.Zoom = 10;
+            this.bunifuImageButton1.Click += new System.EventHandler(this.bunifuImageButton1_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::MediationDB.Properties.Resources.UNHABITAT_svg;
+            this.pictureBox1.Location = new System.Drawing.Point(12, 3);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(203, 39);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 147;
+            this.pictureBox1.TabStop = false;
+            // 
             // frm_beneficiaires
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 17F);
@@ -386,13 +399,14 @@
             this.Controls.Add(this.panel1);
             this.Font = new System.Drawing.Font("Century Gothic", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
-            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "frm_beneficiaires";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.panel1.ResumeLayout(false);
             this.panel1.PerformLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.bunifuImageButton1)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.bunifuCustomDataGrid2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bunifuImageButton1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -414,5 +428,6 @@
         public Bunifu.Framework.UI.BunifuMaterialTextbox txt_adresse;
         public Bunifu.Framework.UI.BunifuMaterialTextbox txt_telephone;
         private Bunifu.Framework.UI.BunifuCustomDataGrid bunifuCustomDataGrid2;
+        private System.Windows.Forms.PictureBox pictureBox1;
     }
 }
