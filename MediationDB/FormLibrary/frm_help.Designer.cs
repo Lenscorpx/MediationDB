f2098be584183c8ad1db67159939beacc2cc42f2
Lens Mutombo <lensmutombo@gmail.com>
2020-03-09 16:34:39 +0200

    changed help

--- MediationDB/FormLibrary/frm_help.Designer.cs
+++ MediationDB/FormLibrary/frm_help.Designer.cs
@@ -47,11 +47,11 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Font = new System.Drawing.Font("Century Gothic", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label2.ForeColor = System.Drawing.Color.White;
-            this.label2.Location = new System.Drawing.Point(23, 71);
+            this.label2.Location = new System.Drawing.Point(59, 71);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(743, 273);
+            this.label2.Size = new System.Drawing.Size(660, 266);
             this.label2.TabIndex = 1;
             this.label2.Text = resources.GetString("label2.Text");
             this.label2.Click += new System.EventHandler(this.label2_Click);
