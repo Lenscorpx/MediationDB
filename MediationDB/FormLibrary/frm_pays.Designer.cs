361c0cbd4338cb1ac36f4bbd201ac2025e06c209
Lens Mutombo <lensmutombo@gmail.com>
2020-03-15 23:52:19 +0200

    Changed font

--- MediationDB/FormLibrary/frm_pays.Designer.cs
+++ MediationDB/FormLibrary/frm_pays.Designer.cs
@@ -28,10 +28,19 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // frm_pays
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 17F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1309, 588);
+            this.Font = new System.Drawing.Font("Century Gothic", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "frm_pays";
             this.Text = "frm_pays";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
