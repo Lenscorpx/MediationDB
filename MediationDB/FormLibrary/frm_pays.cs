3dd95941670bcd11318ded563b524b57425c9e7b
Lens Mutombo <lensmutombo@gmail.com>
2020-03-17 23:45:30 +0200

    Update frm_pays.cs
    call de la methode afficher_pays

--- MediationDB/FormLibrary/frm_pays.cs
+++ MediationDB/FormLibrary/frm_pays.cs
@@ -7,14 +7,21 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using MediationDB.DataLibrary;;
 
 namespace MediationDB.FormLibrary
 {
     public partial class frm_pays : Form
     {
+        Data_Repository rps = new Data_Repository();
         public frm_pays()
         {
             InitializeComponent();
+            refresh();
+        }
+        public void refresh()
+        {
+            rps.afficher_pays(bunifuCustomDataGrid2);
         }
     }
 }
