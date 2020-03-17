00ee1a8139be97b527aed0cdd770bb899a0ea5b5
Lens Mutombo <lensmutombo@gmail.com>
2020-03-17 23:47:55 +0200

    Update frm_pays.cs
    Bug fixed on repository

--- MediationDB/FormLibrary/frm_pays.cs
+++ MediationDB/FormLibrary/frm_pays.cs
@@ -7,7 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-using MediationDB.DataLibrary;;
+using MediationDB.DataLibrary;
 
 namespace MediationDB.FormLibrary
 {
