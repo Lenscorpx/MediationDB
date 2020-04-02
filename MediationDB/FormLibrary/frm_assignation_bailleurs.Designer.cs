b321e5059359aeee91ea5910df094e09631dff7d
Lens Mutombo <lensmutombo@gmail.com>
2020-04-02 23:42:29 +0200

    Update frm_assignation_bailleurs.Designer.cs

--- MediationDB/FormLibrary/frm_assignation_bailleurs.Designer.cs
+++ MediationDB/FormLibrary/frm_assignation_bailleurs.Designer.cs
@@ -34,6 +34,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 17F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(3)))), ((int)(((byte)(43)))), ((int)(((byte)(69)))));
             this.ClientSize = new System.Drawing.Size(1370, 588);
             this.Font = new System.Drawing.Font("Century Gothic", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
