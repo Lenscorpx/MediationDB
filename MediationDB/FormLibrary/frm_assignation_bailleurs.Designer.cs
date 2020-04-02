d5464e583815f2c4e03a9d3100e95e6a86462ad7
Lens Mutombo <lensmutombo@gmail.com>
2020-04-02 23:41:30 +0200

    Update frm_assignation_bailleurs.Designer.cs

--- MediationDB/FormLibrary/frm_assignation_bailleurs.Designer.cs
+++ MediationDB/FormLibrary/frm_assignation_bailleurs.Designer.cs
@@ -39,6 +39,7 @@
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "frm_assignation_bailleurs";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.ResumeLayout(false);
 
         }
