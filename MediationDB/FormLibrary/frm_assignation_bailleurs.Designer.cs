805b1ea17fb64804c1c6a2641388fb00733f9adc
Lens Mutombo <lensmutombo@gmail.com>
2020-04-02 23:41:20 +0200

    Update frm_assignation_bailleurs.Designer.cs

--- MediationDB/FormLibrary/frm_assignation_bailleurs.Designer.cs
+++ MediationDB/FormLibrary/frm_assignation_bailleurs.Designer.cs
@@ -32,11 +32,13 @@
             // 
             // frm_assignation_bailleurs
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 17F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1260, 450);
+            this.ClientSize = new System.Drawing.Size(1370, 588);
+            this.Font = new System.Drawing.Font("Century Gothic", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "frm_assignation_bailleurs";
-            this.Text = "frm_assignation_bailleurs";
             this.ResumeLayout(false);
 
         }
