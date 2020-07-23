5fd2a8ddd17831a1cce0f1d2dc32198ce415d82b
Lens Mutombo <lensmutombo@gmail.com>
2020-07-23 22:54:54 +0200

    Update frm_sensibilises.Designer.cs

--- MediationDB/ReportsLibrary/frm_sensibilises.Designer.cs
+++ MediationDB/ReportsLibrary/frm_sensibilises.Designer.cs
@@ -295,6 +295,8 @@
             // 
             // label1
             // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Century Gothic", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.ForeColor = System.Drawing.Color.White;
@@ -307,6 +309,7 @@
             // documentViewer1
             // 
             this.documentViewer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.documentViewer1.IsMetric = false;
             this.documentViewer1.Location = new System.Drawing.Point(0, 208);
             this.documentViewer1.Name = "documentViewer1";
             this.documentViewer1.Size = new System.Drawing.Size(1300, 479);
