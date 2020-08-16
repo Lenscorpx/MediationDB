9582764d830284b3bcebfdb316af4fda39719144
Lens Mutombo <lensmutombo@gmail.com>
2020-08-16 23:47:21 +0200

    Update uc_test.Designer.cs

--- MediationDB/ControlLibrary/uc_test.Designer.cs
+++ MediationDB/ControlLibrary/uc_test.Designer.cs
@@ -28,8 +28,16 @@
         /// </summary>
         private void InitializeComponent()
         {
-            components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // uc_test
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Name = "uc_test";
+            this.Size = new System.Drawing.Size(1131, 541);
+            this.ResumeLayout(false);
+
         }
 
         #endregion
